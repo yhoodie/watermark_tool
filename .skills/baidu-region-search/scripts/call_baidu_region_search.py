yhoodie/audio_watermark

Initial miaoda project setup with React TypeScript Vite template ffbb4dcb512dfea7ca54bd4188d5cc78f5a7c448 no sync

.skills/baidu-region-search/scripts/call_baidu_region_search.py
@@ -1,0 +1,96 @@
+#!/usr/bin/env python3
+"""
+Query Baidu Map administrative region info.
+
+Usage:
+    python3 call_baidu_region_search.py --keyword "山东省" \
+        [--sub-admin 0] [--extensions-code 0] [--boundary 0] [--boundarycode "370000"]
+
+Environment:
+    INTEGRATIONS_API_KEY - platform-injected API key (required)
+
+Exit codes:
+    0 - success, prints one-line JSON: {"status":"succeed","result":{...}}
+    1 - API or argument error, prints one-line JSON on stderr
+"""
+
+import argparse
+import json
+import os
+import sys
+import urllib.error
+import urllib.parse
+import urllib.request
+
+
+ENDPOINT = "https://app-dyjft4hsvfgh-api-DYJwn2VZXvEa-gateway.appmiaoda.com/api_region_search/v1/"
+
+
+def fail(message):
+    """打印错误信息并以非零状态退出。"""
+    print(message, file=sys.stderr)
+    sys.exit(1)
+
+
+def parse_args():
+    """解析命令行参数。"""
+    p = argparse.ArgumentParser(description="Baidu region search")
+    p.add_argument("--keyword", required=True, help="行政区名称或 adcode")
+    p.add_argument("--sub-admin", default="0", help="0-3 返回下级层级数")
+    p.add_argument("--extensions-code", default="0", help="是否返回国标行政区划编码 0/1")
+    p.add_argument("--boundary", default="0", help="是否返回边界数据 0/1")
+    p.add_argument("--boundarycode", help="需要返回边界的行政区划编码")
+    p.add_argument("--timeout", type=int, default=600, help="request timeout in seconds")
+    return p.parse_args()
+
+
+def call_api(args):
+    """调用上游接口并返回响应。"""
+    api_key = os.environ.get("INTEGRATIONS_API_KEY")
+    if not api_key:
+        fail("INTEGRATIONS_API_KEY is required")
+
+    params = {
+        "keyword": args.keyword,
+        "sub_admin": args.sub_admin,
+        "extensions_code": args.extensions_code,
+        "boundary": args.boundary,
+    }
+    if args.boundarycode:
+        params["boundarycode"] = args.boundarycode
+
+    url = ENDPOINT + "?" + urllib.parse.urlencode(params)
+    req = urllib.request.Request(
+        url,
+        method="GET",
+        headers={
+            "Accept": "application/json",
+            "X-Gateway-Authorization": "Bearer " + api_key,
+        },
+    )
+    try:
+        with urllib.request.urlopen(req, timeout=args.timeout) as resp:
+            body = resp.read().decode("utf-8", errors="replace")
+    except urllib.error.HTTPError as exc:
+        detail = exc.read().decode("utf-8", errors="replace")
+        fail("HTTP %s: %s" % (exc.code, detail[:1000]))
+    except urllib.error.URLError as exc:
+        fail("Network error: " + str(exc.reason))
+
+    try:
+        return json.loads(body)
+    except json.JSONDecodeError:
+        fail("Response is not valid JSON: " + body[:500])
+
+
+def main():
+    """入口：查询行政区划信息。"""
+    args = parse_args()
+    result = call_api(args)
+    if result.get("status") not in (0, None):
+        fail("API error %s" % result.get("status"))
+    print(json.dumps({"status": "succeed", "result": result}, ensure_ascii=False, separators=(",", ":")))
+
+
+if __name__ == "__main__":
+    main()

.skills/baidu-weather-query/scripts/call_weather_query.py
@@ -1,0 +1,97 @@
+#!/usr/bin/env python3
+"""
+Query domestic weather via Baidu Maps weather API, by district code or by lat/lng.
+
+Usage:
+    # By administrative district code
+    python3 call_weather_query.py district --district-id 110100 [--data-type all]
+
+    # By latitude/longitude
+    python3 call_weather_query.py location --latitude 39.915 --longitude 116.404 [--data-type all]
+
+Environment:
+    INTEGRATIONS_API_KEY - platform-injected API key (required)
+
+Exit codes:
+    0 - success, prints one-line JSON: {"status": "succeed", "result": {...}}
+    1 - API or argument error, prints one-line JSON: {"status": "error", "message": "..."}
+"""
+
+import os
+import sys
+import json
+import argparse
+import urllib.request
+import urllib.parse
+import urllib.error
+
+
+DISTRICT_ENDPOINT = "https://app-dyjft4hsvfgh-api-oLpZbd8ed8wa-gateway.appmiaoda.com/weather/v1/"
+LOCATION_ENDPOINT = "https://app-dyjft4hsvfgh-api-GYX1bnRz2Pxa-gateway.appmiaoda.com/weather/v1/"
+
+
+def parse_args():
+    """解析命令行参数。"""
+    p = argparse.ArgumentParser()
+    sub = p.add_subparsers(dest="mode", required=True)
+
+    d = sub.add_parser("district", help="按行政区划代码查询天气")
+    d.add_argument("--district-id", dest="district_id", required=True, help="行政区划代码，如 110100（北京市）")
+    d.add_argument("--data-type", dest="data_type", default="all",
+                    choices=["all", "now", "forecast"], help="数据类型，默认 all")
+
+    l = sub.add_parser("location", help="按经纬度查询天气")
+    l.add_argument("--latitude", required=True, type=float, help="纬度")
+    l.add_argument("--longitude", required=True, type=float, help="经度")
+    l.add_argument("--data-type", dest="data_type", default="all",
+                    choices=["all", "now", "forecast", "hourly"], help="数据类型，默认 all")
+
+    return p.parse_args()
+
+
+def fail(message: str):
+    """打印错误 JSON 并以非零状态退出。"""
+    print(json.dumps({"status": "error", "message": message}))
+    sys.exit(1)
+
+
+def main():
+    """入口：按 mode 调用天气查询接口并输出结果 JSON。"""
+    args = parse_args()
+    api_key = os.environ.get("INTEGRATIONS_API_KEY", "")
+    if not api_key:
+        fail("INTEGRATIONS_API_KEY not set")
+
+    if args.mode == "district":
+        endpoint = DISTRICT_ENDPOINT
+        params = {"district_id": args.district_id, "data_type": args.data_type}
+    else:
+        endpoint = LOCATION_ENDPOINT
+        # 注意：百度地图 API location 参数格式为 纬度,经度
+        params = {"location": f"{args.latitude},{args.longitude}", "data_type": args.data_type}
+
+    url = f"{endpoint}?{urllib.parse.urlencode(params)}"
+    req = urllib.request.Request(
+        url,
+        headers={
+            "Accept": "application/json",
+            "X-Gateway-Authorization": f"Bearer {api_key}",
+        },
+    )
+
+    try:
+        with urllib.request.urlopen(req) as resp:
+            body = json.loads(resp.read())
+    except urllib.error.HTTPError as e:
+        fail(f"HTTP {e.code}: {e.read().decode(errors='replace')}")
+    except urllib.error.URLError as e:
+        fail(f"Network error: {e}")
+
+    if body.get("status") != 0:
+        fail(f"API error status: {body.get('status')}, message: {body.get('message', '')}")
+
+    print(json.dumps({"status": "succeed", "result": body.get("result", body)}))
+
+
+if __name__ == "__main__":
+    main()

.skills/driving-exam-questions/scripts/query_driving_exam_questions.py
@@ -1,0 +1,71 @@
+#!/usr/bin/env python3
+"""查询驾考题库试题。"""
+import argparse
+import json
+import os
+import sys
+import urllib.error
+import urllib.parse
+import urllib.request
+
+
+ENDPOINT = "https://app-dyjft4hsvfgh-api-o9wN0mk1DAPa-gateway.appmiaoda.com/driverexam/query"
+
+
+def call_api(params):
+    """调用上游接口并返回响应。"""
+    api_key = os.environ.get("INTEGRATIONS_API_KEY")
+    if not api_key:
+        raise RuntimeError("INTEGRATIONS_API_KEY is required")
+
+    query = urllib.parse.urlencode(params)
+    request = urllib.request.Request(
+        ENDPOINT + "?" + query,
+        data=b"",
+        method="POST",
+        headers={
+            "Content-Type": "application/json;charset=UTF-8",
+            "X-Gateway-Authorization": "Bearer " + api_key,
+        },
+    )
+    with urllib.request.urlopen(request, timeout=60) as response:
+        body = response.read().decode("utf-8")
+    return json.loads(body)
+
+
+def main():
+    """入口：解析参数，调用接口并输出结果 JSON。"""
+    parser = argparse.ArgumentParser(description="Query driving exam questions.")
+    parser.add_argument("--type", required=True, help="Vehicle type, for example C1, C2, B2")
+    parser.add_argument("--subject", default="1", help="Subject: 1 or 4")
+    parser.add_argument("--pagesize", default="1", help="Page size")
+    parser.add_argument("--pagenum", help="Page number")
+    parser.add_argument("--sort", default="normal", help="Sort mode: normal or rand")
+    parser.add_argument("--chapter", help="Chapter number")
+    args = parser.parse_args()
+
+    params = {
+        "type": args.type,
+        "subject": args.subject,
+        "pagesize": args.pagesize,
+        "sort": args.sort,
+    }
+    if args.pagenum:
+        params["pagenum"] = args.pagenum
+    if args.chapter:
+        params["chapter"] = args.chapter
+
+    try:
+        result = call_api(params)
+        print(json.dumps(result, ensure_ascii=False, separators=(",", ":")))
+    except urllib.error.HTTPError as exc:
+        detail = exc.read().decode("utf-8", errors="replace")
+        print("HTTP error {}: {}".format(exc.code, detail), file=sys.stderr)
+        sys.exit(1)
+    except Exception as exc:
+        print("Error: {}".format(exc), file=sys.stderr)
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()

.skills/minimax-tts/scripts/synthesize_speech.py
@@ -1,0 +1,131 @@
+#!/usr/bin/env python3
+"""
+Generate speech via MiniMax T2A V2 and save the audio to disk.
+Audio URL or hex payload handling stays inside this script.
+
+Usage:
+    python3 synthesize_speech.py --text "你好" --output /path/to/audio.mp3
+
+Environment:
+    INTEGRATIONS_API_KEY - platform-injected API key (required)
+
+Exit codes:
+    0 - success, prints JSON: {"file": "...", "audio_length": ..., "usage_characters": ...}
+    1 - API or argument error
+"""
+
+import os
+import sys
+import json
+import argparse
+import urllib.request
+import urllib.error
+
+
+T2A_URL = "https://app-dyjft4hsvfgh-api-DLEO7Bj0lORa-gateway.appmiaoda.com/v1/t2a_v2"
+
+
+def parse_args():
+    """解析命令行参数。"""
+    p = argparse.ArgumentParser()
+    p.add_argument("--text", required=True)
+    p.add_argument("--output", required=True)
+    p.add_argument("--model", default="speech-02-hd")
+    p.add_argument("--voice-id", default="male-qn-qingse")
+    p.add_argument("--speed", type=float, default=1.0)
+    p.add_argument("--vol", type=float, default=1.0)
+    p.add_argument("--pitch", type=int, default=0)
+    p.add_argument("--format", default="mp3", choices=["mp3", "wav", "flac", "pcm"])
+    p.add_argument("--output-format", default="url", choices=["url", "hex"])
+    return p.parse_args()
+
+
+def post_json(api_key: str, payload_obj: dict) -> dict:
+    """调用上游接口并返回响应。"""
+    payload = json.dumps(payload_obj).encode()
+    req = urllib.request.Request(
+        T2A_URL,
+        data=payload,
+        headers={
+            "Content-Type": "application/json",
+            "X-Gateway-Authorization": f"Bearer {api_key}",
+        },
+    )
+    try:
+        with urllib.request.urlopen(req) as resp:
+            return json.loads(resp.read())
+    except urllib.error.HTTPError as e:
+        body = e.read().decode(errors="replace")
+        print(f"HTTP {e.code}: {body}", file=sys.stderr)
+        sys.exit(1)
+    except urllib.error.URLError as e:
+        print(f"Network error: {e}", file=sys.stderr)
+        sys.exit(1)
+
+
+def download(url: str, output: str):
+    """下载文件并保存到本地路径。"""
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as resp, open(output, "wb") as f:
+        f.write(resp.read())
+
+
+def save_hex(hex_audio: str, output: str):
+    """将十六进制音频数据解码并保存为本地文件。"""
+    with open(output, "wb") as f:
+        f.write(bytes.fromhex(hex_audio))
+
+
+def main():
+    """入口：合成语音并保存音频文件。"""
+    args = parse_args()
+    api_key = os.environ.get("INTEGRATIONS_API_KEY", "")
+    if not api_key:
+        print("INTEGRATIONS_API_KEY not set", file=sys.stderr)
+        sys.exit(1)
+
+    payload = {
+        "model": args.model,
+        "text": args.text,
+        "stream": False,
+        "output_format": args.output_format,
+        "voice_setting": {
+            "voice_id": args.voice_id,
+            "speed": args.speed,
+            "vol": args.vol,
+            "pitch": args.pitch,
+        },
+        "audio_setting": {"format": args.format},
+    }
+
+    d = post_json(api_key, payload)
+    base_resp = d.get("base_resp", {})
+    if base_resp.get("status_code") not in (None, 0):
+        print(f"API error: {json.dumps(d, ensure_ascii=False)}", file=sys.stderr)
+        sys.exit(1)
+
+    data = d.get("data", {})
+    audio = data.get("audio")
+    if not audio:
+        print(f"响应中未找到音频: {json.dumps(d, ensure_ascii=False)}", file=sys.stderr)
+        sys.exit(1)
+
+    if args.output_format == "url":
+        download(audio, args.output)
+        url = audio
+    else:
+        save_hex(audio, args.output)
+        url = None
+
+    extra_info = d.get("extra_info", {})
+    print(json.dumps({
+        "file": args.output,
+        "url": url,
+        "audio_length": extra_info.get("audio_length"),
+        "usage_characters": extra_info.get("usage_characters"),
+        "base_resp": base_resp,
+    }, ensure_ascii=False))
+
+
+if __name__ == "__main__":
+    main()

yhoodie/watermark_tool

.skills/baidu-region-search/scripts/call_baidu_region_search.py
@@ -1,0 +1,96 @@
+#!/usr/bin/env python3
+"""
+Query Baidu Map administrative region info.
+
+Usage:
+    python3 call_baidu_region_search.py --keyword "山东省" \
+        [--sub-admin 0] [--extensions-code 0] [--boundary 0] [--boundarycode "370000"]
+
+Environment:
+    INTEGRATIONS_API_KEY - platform-injected API key (required)
+
+Exit codes:
+    0 - success, prints one-line JSON: {"status":"succeed","result":{...}}
+    1 - API or argument error, prints one-line JSON on stderr
+"""
+
+import argparse
+import json
+import os
+import sys
+import urllib.error
+import urllib.parse
+import urllib.request
+
+
+ENDPOINT = "https://app-dysgncsaheyp-api-DYJwn2VZXvEa-gateway.appmiaoda.com/api_region_search/v1/"
+
+
+def fail(message):
+    """打印错误信息并以非零状态退出。"""
+    print(message, file=sys.stderr)
+    sys.exit(1)
+
+
+def parse_args():
+    """解析命令行参数。"""
+    p = argparse.ArgumentParser(description="Baidu region search")
+    p.add_argument("--keyword", required=True, help="行政区名称或 adcode")
+    p.add_argument("--sub-admin", default="0", help="0-3 返回下级层级数")
+    p.add_argument("--extensions-code", default="0", help="是否返回国标行政区划编码 0/1")
+    p.add_argument("--boundary", default="0", help="是否返回边界数据 0/1")
+    p.add_argument("--boundarycode", help="需要返回边界的行政区划编码")
+    p.add_argument("--timeout", type=int, default=600, help="request timeout in seconds")
+    return p.parse_args()
+
+
+def call_api(args):
+    """调用上游接口并返回响应。"""
+    api_key = os.environ.get("INTEGRATIONS_API_KEY")
+    if not api_key:
+        fail("INTEGRATIONS_API_KEY is required")
+
+    params = {
+        "keyword": args.keyword,
+        "sub_admin": args.sub_admin,
+        "extensions_code": args.extensions_code,
+        "boundary": args.boundary,
+    }
+    if args.boundarycode:
+        params["boundarycode"] = args.boundarycode
+
+    url = ENDPOINT + "?" + urllib.parse.urlencode(params)
+    req = urllib.request.Request(
+        url,
+        method="GET",
+        headers={
+            "Accept": "application/json",
+            "X-Gateway-Authorization": "Bearer " + api_key,
+        },
+    )
+    try:
+        with urllib.request.urlopen(req, timeout=args.timeout) as resp:
+            body = resp.read().decode("utf-8", errors="replace")
+    except urllib.error.HTTPError as exc:
+        detail = exc.read().decode("utf-8", errors="replace")
+        fail("HTTP %s: %s" % (exc.code, detail[:1000]))
+    except urllib.error.URLError as exc:
+        fail("Network error: " + str(exc.reason))
+
+    try:
+        return json.loads(body)
+    except json.JSONDecodeError:
+        fail("Response is not valid JSON: " + body[:500])
+
+
+def main():
+    """入口：查询行政区划信息。"""
+    args = parse_args()
+    result = call_api(args)
+    if result.get("status") not in (0, None):
+        fail("API error %s" % result.get("status"))
+    print(json.dumps({"status": "succeed", "result": result}, ensure_ascii=False, separators=(",", ":")))
+
+
+if __name__ == "__main__":
+    main()

.skills/baidu-weather-query/scripts/call_weather_query.py
@@ -1,0 +1,97 @@
+#!/usr/bin/env python3
+"""
+Query domestic weather via Baidu Maps weather API, by district code or by lat/lng.
+
+Usage:
+    # By administrative district code
+    python3 call_weather_query.py district --district-id 110100 [--data-type all]
+
+    # By latitude/longitude
+    python3 call_weather_query.py location --latitude 39.915 --longitude 116.404 [--data-type all]
+
+Environment:
+    INTEGRATIONS_API_KEY - platform-injected API key (required)
+
+Exit codes:
+    0 - success, prints one-line JSON: {"status": "succeed", "result": {...}}
+    1 - API or argument error, prints one-line JSON: {"status": "error", "message": "..."}
+"""
+
+import os
+import sys
+import json
+import argparse
+import urllib.request
+import urllib.parse
+import urllib.error
+
+
+DISTRICT_ENDPOINT = "https://app-dysgncsaheyp-api-oLpZbd8ed8wa-gateway.appmiaoda.com/weather/v1/"
+LOCATION_ENDPOINT = "https://app-dysgncsaheyp-api-GYX1bnRz2Pxa-gateway.appmiaoda.com/weather/v1/"
+
+
+def parse_args():
+    """解析命令行参数。"""
+    p = argparse.ArgumentParser()
+    sub = p.add_subparsers(dest="mode", required=True)
+
+    d = sub.add_parser("district", help="按行政区划代码查询天气")
+    d.add_argument("--district-id", dest="district_id", required=True, help="行政区划代码，如 110100（北京市）")
+    d.add_argument("--data-type", dest="data_type", default="all",
+                    choices=["all", "now", "forecast"], help="数据类型，默认 all")
+
+    l = sub.add_parser("location", help="按经纬度查询天气")
+    l.add_argument("--latitude", required=True, type=float, help="纬度")
+    l.add_argument("--longitude", required=True, type=float, help="经度")
+    l.add_argument("--data-type", dest="data_type", default="all",
+                    choices=["all", "now", "forecast", "hourly"], help="数据类型，默认 all")
+
+    return p.parse_args()
+
+
+def fail(message: str):
+    """打印错误 JSON 并以非零状态退出。"""
+    print(json.dumps({"status": "error", "message": message}))
+    sys.exit(1)
+
+
+def main():
+    """入口：按 mode 调用天气查询接口并输出结果 JSON。"""
+    args = parse_args()
+    api_key = os.environ.get("INTEGRATIONS_API_KEY", "")
+    if not api_key:
+        fail("INTEGRATIONS_API_KEY not set")
+
+    if args.mode == "district":
+        endpoint = DISTRICT_ENDPOINT
+        params = {"district_id": args.district_id, "data_type": args.data_type}
+    else:
+        endpoint = LOCATION_ENDPOINT
+        # 注意：百度地图 API location 参数格式为 纬度,经度
+        params = {"location": f"{args.latitude},{args.longitude}", "data_type": args.data_type}
+
+    url = f"{endpoint}?{urllib.parse.urlencode(params)}"
+    req = urllib.request.Request(
+        url,
+        headers={
+            "Accept": "application/json",
+            "X-Gateway-Authorization": f"Bearer {api_key}",
+        },
+    )
+
+    try:
+        with urllib.request.urlopen(req) as resp:
+            body = json.loads(resp.read())
+    except urllib.error.HTTPError as e:
+        fail(f"HTTP {e.code}: {e.read().decode(errors='replace')}")
+    except urllib.error.URLError as e:
+        fail(f"Network error: {e}")
+
+    if body.get("status") != 0:
+        fail(f"API error status: {body.get('status')}, message: {body.get('message', '')}")
+
+    print(json.dumps({"status": "succeed", "result": body.get("result", body)}))
+
+
+if __name__ == "__main__":
+    main()

.skills/driving-exam-questions/scripts/query_driving_exam_questions.py
@@ -1,0 +1,71 @@
+#!/usr/bin/env python3
+"""查询驾考题库试题。"""
+import argparse
+import json
+import os
+import sys
+import urllib.error
+import urllib.parse
+import urllib.request
+
+
+ENDPOINT = "https://app-dysgncsaheyp-api-o9wN0mk1DAPa-gateway.appmiaoda.com/driverexam/query"
+
+
+def call_api(params):
+    """调用上游接口并返回响应。"""
+    api_key = os.environ.get("INTEGRATIONS_API_KEY")
+    if not api_key:
+        raise RuntimeError("INTEGRATIONS_API_KEY is required")
+
+    query = urllib.parse.urlencode(params)
+    request = urllib.request.Request(
+        ENDPOINT + "?" + query,
+        data=b"",
+        method="POST",
+        headers={
+            "Content-Type": "application/json;charset=UTF-8",
+            "X-Gateway-Authorization": "Bearer " + api_key,
+        },
+    )
+    with urllib.request.urlopen(request, timeout=60) as response:
+        body = response.read().decode("utf-8")
+    return json.loads(body)
+
+
+def main():
+    """入口：解析参数，调用接口并输出结果 JSON。"""
+    parser = argparse.ArgumentParser(description="Query driving exam questions.")
+    parser.add_argument("--type", required=True, help="Vehicle type, for example C1, C2, B2")
+    parser.add_argument("--subject", default="1", help="Subject: 1 or 4")
+    parser.add_argument("--pagesize", default="1", help="Page size")
+    parser.add_argument("--pagenum", help="Page number")
+    parser.add_argument("--sort", default="normal", help="Sort mode: normal or rand")
+    parser.add_argument("--chapter", help="Chapter number")
+    args = parser.parse_args()
+
+    params = {
+        "type": args.type,
+        "subject": args.subject,
+        "pagesize": args.pagesize,
+        "sort": args.sort,
+    }
+    if args.pagenum:
+        params["pagenum"] = args.pagenum
+    if args.chapter:
+        params["chapter"] = args.chapter
+
+    try:
+        result = call_api(params)
+        print(json.dumps(result, ensure_ascii=False, separators=(",", ":")))
+    except urllib.error.HTTPError as exc:
+        detail = exc.read().decode("utf-8", errors="replace")
+        print("HTTP error {}: {}".format(exc.code, detail), file=sys.stderr)
+        sys.exit(1)
+    except Exception as exc:
+        print("Error: {}".format(exc), file=sys.stderr)
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()

.skills/minimax-tts/scripts/synthesize_speech.py
@@ -1,0 +1,131 @@
+#!/usr/bin/env python3
+"""
+Generate speech via MiniMax T2A V2 and save the audio to disk.
+Audio URL or hex payload handling stays inside this script.
+
+Usage:
+    python3 synthesize_speech.py --text "你好" --output /path/to/audio.mp3
+
+Environment:
+    INTEGRATIONS_API_KEY - platform-injected API key (required)
+
+Exit codes:
+    0 - success, prints JSON: {"file": "...", "audio_length": ..., "usage_characters": ...}
+    1 - API or argument error
+"""
+
+import os
+import sys
+import json
+import argparse
+import urllib.request
+import urllib.error
+
+
+T2A_URL = "https://app-dysgncsaheyp-api-DLEO7Bj0lORa-gateway.appmiaoda.com/v1/t2a_v2"
+
+
+def parse_args():
+    """解析命令行参数。"""
+    p = argparse.ArgumentParser()
+    p.add_argument("--text", required=True)
+    p.add_argument("--output", required=True)
+    p.add_argument("--model", default="speech-02-hd")
+    p.add_argument("--voice-id", default="male-qn-qingse")
+    p.add_argument("--speed", type=float, default=1.0)
+    p.add_argument("--vol", type=float, default=1.0)
+    p.add_argument("--pitch", type=int, default=0)
+    p.add_argument("--format", default="mp3", choices=["mp3", "wav", "flac", "pcm"])
+    p.add_argument("--output-format", default="url", choices=["url", "hex"])
+    return p.parse_args()
+
+
+def post_json(api_key: str, payload_obj: dict) -> dict:
+    """调用上游接口并返回响应。"""
+    payload = json.dumps(payload_obj).encode()
+    req = urllib.request.Request(
+        T2A_URL,
+        data=payload,
+        headers={
+            "Content-Type": "application/json",
+            "X-Gateway-Authorization": f"Bearer {api_key}",
+        },
+    )
+    try:
+        with urllib.request.urlopen(req) as resp:
+            return json.loads(resp.read())
+    except urllib.error.HTTPError as e:
+        body = e.read().decode(errors="replace")
+        print(f"HTTP {e.code}: {body}", file=sys.stderr)
+        sys.exit(1)
+    except urllib.error.URLError as e:
+        print(f"Network error: {e}", file=sys.stderr)
+        sys.exit(1)
+
+
+def download(url: str, output: str):
+    """下载文件并保存到本地路径。"""
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as resp, open(output, "wb") as f:
+        f.write(resp.read())
+
+
+def save_hex(hex_audio: str, output: str):
+    """将十六进制音频数据解码并保存为本地文件。"""
+    with open(output, "wb") as f:
+        f.write(bytes.fromhex(hex_audio))
+
+
+def main():
+    """入口：合成语音并保存音频文件。"""
+    args = parse_args()
+    api_key = os.environ.get("INTEGRATIONS_API_KEY", "")
+    if not api_key:
+        print("INTEGRATIONS_API_KEY not set", file=sys.stderr)
+        sys.exit(1)
+
+    payload = {
+        "model": args.model,
+        "text": args.text,
+        "stream": False,
+        "output_format": args.output_format,
+        "voice_setting": {
+            "voice_id": args.voice_id,
+            "speed": args.speed,
+            "vol": args.vol,
+            "pitch": args.pitch,
+        },
+        "audio_setting": {"format": args.format},
+    }
+
+    d = post_json(api_key, payload)
+    base_resp = d.get("base_resp", {})
+    if base_resp.get("status_code") not in (None, 0):
+        print(f"API error: {json.dumps(d, ensure_ascii=False)}", file=sys.stderr)
+        sys.exit(1)
+
+    data = d.get("data", {})
+    audio = data.get("audio")
+    if not audio:
+        print(f"响应中未找到音频: {json.dumps(d, ensure_ascii=False)}", file=sys.stderr)
+        sys.exit(1)
+
+    if args.output_format == "url":
+        download(audio, args.output)
+        url = audio
+    else:
+        save_hex(audio, args.output)
+        url = None
+
+    extra_info = d.get("extra_info", {})
+    print(json.dumps({
+        "file": args.output,
+        "url": url,
+        "audio_length": extra_info.get("audio_length"),
+        "usage_characters": extra_info.get("usage_characters"),
+        "base_resp": base_resp,
+    }, ensure_ascii=False))
+
+
+if __name__ == "__main__":
+    main()